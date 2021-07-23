5786742b1c4d3a226f2c7c4bf9ae734562123f1d
Owen Jones <owenthefirst93@gmail.com>
2021-07-23 15:21:23 -0400

    mongo db product model

--- db/index.js
+++ db/index.js
@@ -1,9 +1,11 @@
-require('dotenv').config();
+//dotenv wont work for some fucking reason 
+
+require('dotenv').config()
 const mongoose = require('mongoose');
 
 const connectDB = async() => {
     try{
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect('mongodb+srv://user-123:user-123@ecommerce-cartel-oj.rxklv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
             useNewUrlParser: true, 
             useUnifiedTopology: true 
         })
@@ -12,6 +14,6 @@ const connectDB = async() => {
         console.log('error',error)
     }
 }
-
-
+// console.log(mongo)
+// console.log(port)
 module.exports = connectDB
