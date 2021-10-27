92a0c4749b42103e899cc2235594c6e4b5885977
Owen Jones <owenthefirst93@gmail.com>
2021-10-27 15:08:49 -0400

    initialize redux store

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom'
+
+import {Provider} from 'react-redux'
+import store from './redux/store'
+
 ReactDOM.render(
   <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <Provider store={store}> 
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
   </Router>,
   document.getElementById('root')
 );
