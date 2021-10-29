c4c8e3d15ea714caf76fa9273aefbb8bf395a3a5
Owen Jones <owenthefirst93@gmail.com>
2021-10-28 23:44:52 -0400

    finish cart Reducer

--- client/src/redux/reducers/cartReducers.js
+++ client/src/redux/reducers/cartReducers.js
@@ -17,7 +17,11 @@ export const cartReducer = (state = {cartItems: [], action}) => {
                     cartItems: [...state.cartItems, item],
                 }
             }
-            // case actionTypes
+            case actionTypes.REMOVE_FROM_CART:
+                return {
+                    ...state,
+                    cartItems: state.cartItems.filter((x) => x.product !== action.payload)
+                }
         default:
             return state;
     }
