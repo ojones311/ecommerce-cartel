3c3b2b3f032f1359f9faa6773388a4c75668917e
Owen Jones <owenthefirst93@gmail.com>
2021-10-30 13:59:50 -0400

    getProductsReducer

--- client/src/redux/reducers/productReducer.js
+++ client/src/redux/reducers/productReducer.js
@@ -4,6 +4,26 @@ const PRODUCT_INITIAL_STATE = {
 
 }
 
-export const productReducer = (state = PRODUCT_INITIAL_STATE, action) => {
-
+export const getProductsReducer = (state = PRODUCT_INITIAL_STATE, action) => {
+    switch(action.type){
+        case actionTypes.GET_PRODUCTS_REQUEST:
+            return {
+                loading: true,
+                products: []
+            }
+        case actionTypes.GET_PRODUCTS_SUCCESS:
+            return {
+                loading: false,
+                products: action.payload
+            }
+        case actionTypes.GET_PRODUCTS_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+        default:
+            return state 
+    }
 }
+
+export const getProductDetails = (state)
