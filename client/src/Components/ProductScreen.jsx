7f28ac265a78d819aee573b8b547305268d137f0
Owen Jones <owenthefirst93@gmail.com>
2021-11-09 21:52:42 -0500

    adding product details

--- client/src/Components/ProductScreen.jsx
+++ client/src/Components/ProductScreen.jsx
@@ -1,6 +1,6 @@
 import '../styles/ProductScreen.css'
 import {useState, useEffect} from 'react'
-import {useDispatch, useSelector} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import {getProductDetails} from '../redux/actions/productActions'
 import {addToCart} from '../redux/actions/cartActions'
 
@@ -17,7 +17,7 @@ const ProductScreen = ({match, history}) => {
             dispatch(getProductDetails(match.params.id))
         }
        
-    },[dispatch,product, match])
+    },[dispatch, product, match])
 
     return (
         <div className='productscreen'>
@@ -25,22 +25,22 @@ const ProductScreen = ({match, history}) => {
                 <>
                     <div className='productscreen_left'>
                 <div className='left_image'>
-                <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt='product name'/>
+                <img src={product.imageUrl} alt={product.name}/>
                 </div>
                 <div className='left_info'>
-                    <p className='left_name'>Product 1</p>
-                    <p>Price 499.99</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptates officiis dicta nihil architecto consequuntur nisi natus esse ad inventore labore tempore reprehenderit eius debitis, numquam incidunt, beatae sunt alias!</p>
+                    <p className='left_name'>{product.name}</p>
+                    <p>${product.price}</p>
+                    <p>Description: {product.description}</p>
                 </div>
             </div>
             <div className='productscreen_right'>
                 <div className='right_image'>
                     <div className='right_info'>
                         <p>
-                            Price <span>$499.99</span>
+                            Price <span>${product.price}</span>
                         </p>
                         <p>
-                            Status: <span>In Stock</span>
+                            Status: <span>{product.countInStock > 0 ? "In Stock" : "Out of Stock" }</span>
                         </p>
                         <p>
                             Quantity
@@ -65,3 +65,4 @@ const ProductScreen = ({match, history}) => {
 }
 
 export default ProductScreen
+
