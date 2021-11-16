efab51ef913b6aafe55483ca3c9134618e28f25a
Owen Jones <owenthefirst93@gmail.com>
2021-11-15 21:25:33 -0500

    get cart count on sode drawer and header icon and cart info persisting from localStorage

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,8 +1,14 @@
 import '../styles/NavBar.css'
 import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 const NavBar = ({click}) => {
+    const cart = useSelector(state => state.cart)
+    const {cartItems} = cart
 
+    const getCartCount = () => {
+        return cartItems.reduce((qty, item) => qty + Number(item.qty), 0)
+    }
     return (
         <nav className= 'navbar'>
             <div className= 'navbar-logo'>
@@ -16,7 +22,7 @@ const NavBar = ({click}) => {
                     <Link to ='/cart' className='cart_link'>
                         <i className='fas fa-shopping-cart'></i>
                         Cart
-                        <span className='cartlogo-badge'>0</span>
+                        <span className='cartlogo-badge'>{getCartCount()}</span>
                     </Link>
                 </li>
                 <li>
--- client/src/Components/SideDrawer.jsx
+++ client/src/Components/SideDrawer.jsx
@@ -1,11 +1,19 @@
 import '../styles/SideDrawer.css'
 import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 const SideDrawer = ({show,click}) => {
 
     const sideDrawerClass = ['sidedrawer']
     if(show){
         sideDrawerClass.push('show')
+    }
+
+    const cart = useSelector(state => state.cart)
+    const {cartItems} = cart
+
+    const getCartCount = () => {
+        return cartItems.reduce((qty, item) => qty + Number(item.qty), 0)
     }
     return(
         <div className={sideDrawerClass.join(' ')}>
@@ -14,7 +22,7 @@ const SideDrawer = ({show,click}) => {
                     <Link to='/cart'>
                         <i className='fas fa-shopping-cart'></i>
                         <span>
-                            Cart <span className='sidedrawwer_cartbadge'>0</span>
+                            Cart <span className='sidedrawwer_cartbadge'>{getCartCount()}</span>
                         </span>
                     </Link>
                 </li>
